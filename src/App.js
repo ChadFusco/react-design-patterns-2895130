125beec213bc077435e47ec24c34e1f9c2a4e355
Chad Fusco <fusco@case.edu>
2023-03-26 13:42:19 -0700

    01_05 complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { LargePersonListItem } from "./people/LargePersonListItem";
 import { SmallPersonListItem } from "./people/SmallPersonListItem";
+import { LargeProductListItem } from "./products/LargeProductListItem";
+import { SmallProductListItem } from "./products/SmallProductListItem";
 import { RegularList } from "./RegularList";
+import { NumberedList } from "./NumberedList";
 
 const people = [{
 	name: 'John Doe',
@@ -39,14 +42,26 @@ const products = [{
 function App() {
 	return (
 		<>
-		<RegularList
-			items={people}
-			resourceName="person"
-			itemComponent={SmallPersonListItem} />
-		<RegularList
-			items={people}
-			resourceName="person"
-			itemComponent={LargePersonListItem} />
+			<RegularList
+				items={people}
+				resourceName="person"
+				itemComponent={SmallPersonListItem}
+			/>
+			<NumberedList
+				items={people}
+				resourceName="person"
+				itemComponent={LargePersonListItem}
+			/>
+			<RegularList
+				items={products}
+				resourceName="product"
+				itemComponent={SmallProductListItem}
+			/>
+			<NumberedList
+				items={products}
+				resourceName="product"
+				itemComponent={LargeProductListItem}
+			/>
 		</>
 	);
 }
